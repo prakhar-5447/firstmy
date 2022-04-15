19d2c3a5a0e0d0fa84042056f9d76f0d262e6b96
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-04-15 16:45:17 +0530

    add login btn

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -23,7 +23,7 @@ function Navbar() {
             <span className="navbar-toggler-icon"></span>
           </button>
           <div className="collapse navbar-collapse" id="navbarNav">
-            <ul className="navbar-nav">
+            <ul className="navbar-nav  me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <Link
                   className={`nav-link ${
@@ -56,6 +56,14 @@ function Navbar() {
                 </Link>
               </li>
             </ul>
+            <form className="d-flex">
+              <Link className="btn btn-primary mx-1" to="/login" role="button">
+                Login
+              </Link>
+              <Link className="btn btn-primary mx-1" to="/signup" role="button">
+                Signup
+              </Link>
+            </form>
           </div>
         </div>
       </nav>
