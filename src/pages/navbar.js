10f603c428e2190f9ba7a79b94466c224b29a4e0
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-04-18 11:11:11 +0530

    navbar

--- src/pages/navbar.js
+++ src/pages/navbar.js
@@ -8,9 +8,9 @@ function Navbar() {
     <>
       <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
-          <Link className="navbar-brand" to="/">
+          {/* <Link className="navbar-brand" to="/">
             Navbar
-          </Link>
+          </Link> */}
           <button
             className="navbar-toggler"
             type="button"
