b661579ebe94b27bbb16f82a0602c64ebd024f24
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-04-28 12:16:37 +0530

    change home page design

--- src/pages/home.js
+++ src/pages/home.js
@@ -6,17 +6,15 @@ function Home() {
   return (
     <>
       <div className="bg-img"></div>
-      <div className="box-main">
+      <div className="box-main d-flex flex-column">
         <div className="firstHalf">
-          <h1 className="text-big" id="head">FirstMy</h1>
-          <hr />
+          <h1 className="text-big">FirstMy</h1>
           <p className="text-small">
-            <br />FirstMy transforms the lives of children in crisis through the
+            FirstMy transforms the lives of children in crisis through the
             healing power of God, family, and community.
           </p>
-          <br />
-          <button className="Explore btn-primary btn-dark btn-lg"><Link className="text-white p-3 t-none"to="./display">Explore &#8594;</Link></button>
         </div>
+        <button className="Explore btn-primary btn-dark btn-lg"><Link className="text-white p-3 t-none" to="./display">Explore &#8594;</Link></button>
       </div>
     </>
   );
