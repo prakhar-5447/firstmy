8adfc431137cf0b8d8d6c5612f6fbf0fa8c494d9
prakhar-5447 <sahuprakhar022003@gmail.com>
2022-04-28 12:33:11 +0530

    modify about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -5,31 +5,33 @@ function About() {
   return (
     <>
       <div className="middle-box">
-        <div className="point-1">
-          <h1 className="topic1">Vision</h1>
-          <p>
-            A strong family for every child. To empower adult orphans with
-            opportunities and support.
-          </p>
-        </div>
+        <div className="overlay">
+          <div className="point-1">
+            <h1 className="topic1">Vision</h1>
+            <p>
+              A strong family for every child. To empower adult orphans with
+              opportunities and support.
+            </p>
+          </div>
 
-        <div className="point-2">
-          <h1 className="topic2">Mission</h1>
-          <p>
-            FirstMy is dedicated to preventing child abuse by strengthening,
-            equipping, and restoring children and families in crisis… “One Life at
-            a Time.”
-          </p>
-        </div>
-        <div className="point-3">
-          <h1 className="topic3">Statement of Faith</h1>
-          <p>
-            FirstMy seeks to make available the teachings of Judeo-Christian
-            principles while respecting the religious background of each
-            individual. We acknowledge the Bible to be the inspired Word of God
-            that teaches there is one Triune God and that salvation is a gift
-            gained through forgiven
-          </p>
+          <div className="point-2">
+            <h1 className="topic2">Mission</h1>
+            <p>
+              FirstMy is dedicated to preventing child abuse by strengthening,
+              equipping, and restoring children and families in crisis… “One Life at
+              a Time.”
+            </p>
+          </div>
+          <div className="point-3">
+            <h1 className="topic3">Statement of Faith</h1>
+            <p>
+              FirstMy seeks to make available the teachings of Judeo-Christian
+              principles while respecting the religious background of each
+              individual. We acknowledge the Bible to be the inspired Word of God
+              that teaches there is one Triune God and that salvation is a gift
+              gained through forgiven
+            </p>
+          </div>
         </div>
       </div>
     </>
